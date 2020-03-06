5a24ac4e6094756f184a58414f019a7c46eb2165
findingamaranth <hurricanehead@gmail.com>
2020-03-06 14:27:59 -0500

    initial commit

---
+++ Definition.java
@@ -0,0 +1,10 @@
+
+public class Definition {
+	public String word;
+	public String definition;
+	
+	public Definition(String word, String definition) {
+		this.word = word;
+		this.definition = definition;
+	}
+}
