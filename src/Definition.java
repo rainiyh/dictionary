c81e0971c182d427e499fa4e15a6ddc2eb4e983d
youseuf2104 <youseuf.molla@ontariotechu.net>
2020-03-12 18:43:22 -0400

    Added Edit Word Definition Function

---
+++ src/Definition.java
@@ -0,0 +1,15 @@
+
+public class Definition {
+	public String word;
+	public String definition;
+	
+	public Definition(String word, String definition) {
+		this.word = word;
+		this.definition = definition;
+	}
+
+
+	public void setDefinition(String def) {
+		this.definition = def;
+	}
+}
