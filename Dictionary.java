4a06403726e0c5ce7f818d7830358b580af7e600
youseuf2104 <youseuf.molla@ontariotechu.net>
2020-03-12 15:29:16 -0400

    Added CSV Reading functionality

--- Dictionary.java
+++ Dictionary.java
@@ -1,17 +1,35 @@
 import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Dictionary {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// Instantiate our dictionary
 		ArrayList<Definition> dictionary = new ArrayList<Definition>();
-		
+
 		// Add definitions to the dictionary
 		dictionary.add(new Definition("school", "learning establishment"));
 		dictionary.add(new Definition("table", "raised flat surface for working or placing objects"));
-		
+
+//parsing a CSV file into Scanner class constructor
+		//THE table.csv text file must be in the same directory as the project, not the actual .java file
+		Scanner sc = new Scanner(new File("table.csv"));
+		sc.useDelimiter(",");   //sets the delimiter pattern
+		int contentNum = 1;
+		while (sc.hasNext())  //returns a boolean value
+
+
+			dictionary.add(new Definition(sc.next(), sc.next()));
+		//find and returns the next complete token from this scanner
+
+
 		// Loop through the dictionary (for each definition object in the dictionary)
 		for (Definition def : dictionary) {
 			System.out.println(def.word + ": " + def.definition);
+
+			sc.close();  //closes the scanner
 		}
 	}
 }
+
