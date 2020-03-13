a89e4608205ca5e89eaed5db2f32b80463351d38
findingamaranth <hurricanehead@gmail.com>
2020-03-12 20:12:24 -0400

    The mighty refactoring

--- Dictionary.java
+++ Dictionary.java
@@ -4,62 +4,74 @@ import java.util.Scanner;
 
 
 public class Dictionary {
-	public static void main(String[] args) throws Exception {
+
+	public static ArrayList<Definition> dictionary;
+
+	public static void main(String[] args) throws IOException {
 		// Instantiate our dictionary as an array list of the class Definitions
-		ArrayList<Definition> dictionary = new ArrayList<Definition>();
+		dictionary = new ArrayList<Definition>();
 
-		// Add definitions to the dictionary
-		dictionary.add(new Definition("school", "learning establishment"));
+		// Add some definitions to the dictionary
 		dictionary.add(new Definition("table", "raised flat surface for working or placing objects"));
+		dictionary.add(new Definition("school", "learning establishment"));
 
-//parsing a CSV file into Scanner class constructor
-		//THE table.csv text file must be in the same directory as the project, not the actual .java file
-		Scanner sc = new Scanner(new File("table.csv"));
-		sc.useDelimiter(",");   //sets the delimiter pattern
-		int contentNum = 1;
-		while (sc.hasNext())  //returns a boolean value
+		// Read from CSV
+		readCSV("table.csv");
 
-
-			dictionary.add(new Definition(sc.next(), sc.next()));
-		//find and returns the next complete token from this scanner
-
-///////////////////////////////////////////////
-		//*THIS IS THE EDIT WORD DEFINITION FUNCTION*
 		//Get the word that we will change from the user
 		Scanner myWord = new Scanner(System.in);  // Create a Scanner object
-		System.out.println("Which word do you want to edit the definition of?:");
+		System.out.println("Which word do you want to edit the definition of?: ");
 		String thisWord = myWord.nextLine();  // Read user input
 
 		//Get the new definition from the user for that word
-		System.out.println("What do you want to set the new definition to:");
-		String thisDef = myWord.nextLine();  // Read user input
+		System.out.println("What do you want to set the new definition to: ");
+		String newDef = myWord.nextLine();  // Read user input
 
-		//Get the index of that specific word in the dictionary
-		int thisIndex = 0;
-		boolean notInDict = true;
-		for (int i = 0; i< dictionary.size();) {
-			if (dictionary.get(i).word.equals(thisWord)) {
-				thisIndex =  i;
-				notInDict = false;
-				break;
-			}
-			i++;
-		}
-		//If that word isn't in the dictionary, then output this
-		if (notInDict == true)
-			System.out.println("That word is not in the dictionary!");
-		//If that word is in the dictionary, then replace the definition with the new inputted one
-		else
-		dictionary.get(thisIndex).setDefinition(thisDef);
+		//Replace the definition
+		updateDef(thisWord, newDef);
 
-		////////////////////////////////////////
-
-
-		// Loop through the dictionary (for each definition object in the dictionary)
+		// Loop through the dictionary and print each word and definition
 		for (Definition def : dictionary) {
 			System.out.println(def.word + ": " + def.definition);
+		}
 
-			sc.close();  //closes the scanner
+		// Close the scanner
+		myWord.close();
+	}
+
+	// Search
+	public static int findWord(String s) {
+		//Get the index of that specific word in the dictionary
+		for (int i = 0; i < dictionary.size(); i++) {
+			if (dictionary.get(i).word.equals(s))
+				return i;
+		}
+		//If that word hasn't been found in the dictionary, return -1
+		return -1;
+	}
+
+	// Read from CSV file
+	public static void readCSV(String csv) throws FileNotFoundException {
+		//parse a CSV file into Scanner class constructor
+		//THE table.csv text file must be in the same directory as the project, not the actual .java file
+		Scanner sc = new Scanner(new File(csv));
+		sc.useDelimiter(",");   //sets the delimiter pattern
+
+		//Scan CSV into tokens, add everything to dictionary
+		while (sc.hasNext())
+			dictionary.add(new Definition(sc.next(), sc.next()));
+
+		// Close the scanner
+		sc.close();
+	}
+
+	public static void updateDef(String word, String newDef) {
+		// Find the index of word and replace its definition
+		if (findWord(word) >= 0) {
+			dictionary.get(findWord(word)).setDefinition(newDef);
+			System.out.println("Definition updated.");
+		} else {
+			System.out.println("Word not found in dictionary!");
 		}
 	}
 }
