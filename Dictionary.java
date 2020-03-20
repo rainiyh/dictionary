fb0aedc4950e4d31a15fbc990a0c2f229b3f8931
findingamaranth <hurricanehead@gmail.com>
2020-03-20 14:50:36 -0400

    added a function that doens't work

--- Dictionary.java
+++ Dictionary.java
@@ -11,16 +11,27 @@ public class Dictionary {
 		// Instantiate our dictionary as an array list of the class Definitions
 		dictionary = new ArrayList<Definition>();
 
-		// Add some definitions to the dictionary
-		dictionary.add(new Definition("table", "raised flat surface for working or placing objects"));
-		dictionary.add(new Definition("school", "learning establishment"));
-
 		// Read from CSV
-		readCSV("table.csv");
+		readCSV("words.txt");
+		
+		// Add scanner
+		Scanner myWord = new Scanner(System.in);  // Create a Scanner object
+		
+		// Get the word to search
+		System.out.println("Search any text: ");
+		String asdf = myWord.nextLine();
+		ArrayList<Definition> list = findList(asdf);
+		for (Definition d : list) {
+			System.out.println(d.word + ": " + d.definition);
+		}
+		
+		/* Get the word to remove
+		System.out.println("Which word to remove: ");
+		String word = myWord.nextLine();
+		removeDef(word);
 
 		//Get the word that we will change from the user
-		Scanner myWord = new Scanner(System.in);  // Create a Scanner object
-		System.out.println("Which word do you want to edit the definition of?: ");
+		System.out.println("Which word do you want to edit the definition of: ");
 		String thisWord = myWord.nextLine();  // Read user input
 
 		//Get the new definition from the user for that word
@@ -33,21 +44,32 @@ public class Dictionary {
 		// Loop through the dictionary and print each word and definition
 		for (Definition def : dictionary) {
 			System.out.println(def.word + ": " + def.definition);
-		}
+		}*/
 
 		// Close the scanner
 		myWord.close();
 	}
 
-	// Search
-	public static int findWord(String s) {
+	public static int findExact(String s) {
 		//Get the index of that specific word in the dictionary
 		for (int i = 0; i < dictionary.size(); i++) {
-			if (dictionary.get(i).word.equals(s))
+			if (dictionary.get(i).word.equalsIgnoreCase(s))
 				return i;
 		}
 		//If that word hasn't been found in the dictionary, return -1
 		return -1;
+	}
+	
+	public static ArrayList<Definition> findList(String s) {
+		// Return the list of all definitions that match the search
+		ArrayList<Definition> subDefinition = new ArrayList<Definition>();
+		for (int i = 0; i < dictionary.size(); i++) {
+			if (dictionary.get(i).word.matches(s)) {
+				subDefinition.add(dictionary.get(i));
+			}
+		}
+		
+		return subDefinition;
 	}
 
 	// Read from CSV file
@@ -55,11 +77,14 @@ public class Dictionary {
 		//parse a CSV file into Scanner class constructor
 		//THE table.csv text file must be in the same directory as the project, not the actual .java file
 		Scanner sc = new Scanner(new File(csv));
-		sc.useDelimiter(",");   //sets the delimiter pattern
+		sc.useDelimiter("\n");   //sets the delimiter pattern
 
 		//Scan CSV into tokens, add everything to dictionary
-		while (sc.hasNext())
-			dictionary.add(new Definition(sc.next(), sc.next()));
+		while (sc.hasNext()) {
+			try {
+				dictionary.add(new Definition(sc.next(), sc.next()));
+			} catch (NoSuchElementException e) {}
+		}
 
 		// Close the scanner
 		sc.close();
@@ -67,12 +92,22 @@ public class Dictionary {
 
 	public static void updateDef(String word, String newDef) {
 		// Find the index of word and replace its definition
-		if (findWord(word) >= 0) {
-			dictionary.get(findWord(word)).setDefinition(newDef);
+		if (findExact(word) >= 0) {
+			dictionary.get(findExact(word)).setDefinition(newDef);
 			System.out.println("Definition updated.");
 		} else {
 			System.out.println("Word not found in dictionary!");
 		}
+	}
+	
+	// Add definition
+	public static void addDef(String word, String newDef) {
+		dictionary.add(new Definition(word, newDef));
+	}
+	
+	// Remove definition
+	public static void removeDef(String word) {
+		dictionary.remove(findExact(word));
 	}
 }
 
